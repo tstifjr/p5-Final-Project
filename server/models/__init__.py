bf13a28e822273b909882b0c21d3a683ba927fd7
TJ Stifter <tjstifter@gmail.com>
2023-08-28 09:28:32 -0400

    added board model

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,5 +1,6 @@
 from .user import User
 from .game import Game
 from .square import Square
+from .board import Board
 
-__all__ = ['User', 'Game', 'Square']
+__all__ = ['User', 'Game', 'Square', 'Board']
